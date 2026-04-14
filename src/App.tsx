0f1dbf93f3d171cebe1350dd14c133287a267be0
Fusan02 <saitoufutoshi0909@gmail.com>
2026-04-14 17:28:58 +0900

    fix: useEffect に直接関数をかくと再利用できなくなるため、useCallBack を用いて再利用可能とパフォーマンス向上を行なった。その際副作用のリスクとして懸念される連鎖レンダリングをfetch関数はデータを返すだけの形に変更し、配列はのちにセットすることで回避している.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 
 type Movie = {
@@ -31,35 +31,40 @@ function App() {
   // 取得した映画リストを保存する配列 
   const [movieList, setMovieList] = useState<Movie[]>([]);
 
-  // useEffect を用いて keywoord が更新されたら際レンダリングをさせる.
+  // useCallback を用いて keyword が更新されない限り同じ関数を使い回すように fetchMovieList関数を定義
+  const fetchMovieList = useCallback(async () => {
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+    let url = "";
+    if (keyword) {
+      url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ja&page=1`;
+    } else {
+      url = "https://api.themoviedb.org/3/movie/popular?language=ja&page=1";
+    }
+    // await でデータが取得されるまで次の処理に進まないようにする.
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    });
+    const data = await response.json();
+    const result = data.results;
+    const movieList = result.map((movie: MovieJson) => ({
+      id: movie.id,
+      original_title: movie.original_title,
+      poster_path: movie.poster_path,
+    }));
+    return movieList;
+  }, [keyword]);
+
+  // useEffect を用いて keyword によって fetchMovieList が更新されたら再レンダリングする.
   useEffect(() => {
-    // movieList を取得してくる関数
-    const fetchMovieList = async () => {
-      const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-      let url = "";
-      if (keyword) {
-        url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ja&page=1`;
-      } else {
-        url = "https://api.themoviedb.org/3/movie/popular?language=ja&page=1";
-      }
-      // await でデータが取得されるまで次の処理に進まないようにする.
-      const response = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${API_KEY}`,
-        },
-      });
-      const data = await response.json();
-      const result = data.results;
-      const movieList = result.map((movie: MovieJson) => ({
-        id: movie.id,
-        original_title: movie.original_title,
-        poster_path: movie.poster_path,
-      }));
+    const loadMovieList = async () => {
+      const movieList = await fetchMovieList();
       setMovieList(movieList);
     };
-    
-    fetchMovieList();
-  }, [keyword]);
+
+    loadMovieList();
+  }, [fetchMovieList]);
 
   return (
     <div>
