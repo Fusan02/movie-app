7ac98bdc54c9c6c1eec7c0bc57616329c2a34458
Fusan02 <saitoufutoshi0909@gmail.com>
2026-04-15 23:46:04 +0900

    add: 映画詳細コンポーネントの作成. fix: リンターの設定を追加.

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,90 @@
-import { useCallback, useEffect, useState } from 'react';
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 type Movie = {
-  id: string;
-  original_title: string;
-  poster_path: string;
-  overview: string;
+    id: string;
+    original_title: string;
+    poster_path: string;
+    overview: string;
 };
 
-type MovieJson ={
-  adult: boolean;
-  backdrop_path: string | null;
-  genre_ids: number[];
-  id: string;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string | null;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
+type MovieJson = {
+    adult: boolean;
+    backdrop_path: string | null;
+    genre_ids: number[];
+    id: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
 };
 
 function App() {
-// 検索キーワードを保存する変数
-  const [keyword, setKeyword] = useState("");
-  // 取得した映画リストを保存する配列 
-  const [movieList, setMovieList] = useState<Movie[]>([]);
+    // 検索キーワードを保存する変数
+    const [keyword, setKeyword] = useState("");
+    // 取得した映画リストを保存する配列
+    const [movieList, setMovieList] = useState<Movie[]>([]);
 
-  // useCallback を用いて keyword が更新されない限り同じ関数を使い回すように fetchMovieList関数を定義
-  const fetchMovieList = useCallback(async () => {
-    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-    let url = "";
-    if (keyword) {
-      url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ja&page=1`;
-    } else {
-      url = "https://api.themoviedb.org/3/movie/popular?language=ja&page=1";
-    }
-    // await でデータが取得されるまで次の処理に進まないようにする.
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-      },
-    });
-    const data = await response.json();
-    const result = data.results;
-    const movieList = result.map((movie: MovieJson) => ({
-      id: movie.id,
-      original_title: movie.original_title,
-      poster_path: movie.poster_path,
-    }));
-    return movieList;
-  }, [keyword]);
+    // useCallback を用いて keyword が更新されない限り同じ関数を使い回すように fetchMovieList関数を定義
+    const fetchMovieList = useCallback(async () => {
+        const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+        let url = "";
+        if (keyword) {
+            url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ja&page=1`;
+        } else {
+            url =
+                "https://api.themoviedb.org/3/movie/popular?language=ja&page=1";
+        }
+        // await でデータが取得されるまで次の処理に進まないようにする.
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+            },
+        });
+        const data = await response.json();
+        const result = data.results;
+        const movieList = result.map((movie: MovieJson) => ({
+            id: movie.id,
+            original_title: movie.original_title,
+            poster_path: movie.poster_path,
+        }));
+        return movieList;
+    }, [keyword]);
 
-  // useEffect を用いて keyword によって fetchMovieList が更新されたら再レンダリングする.
-  useEffect(() => {
-    const loadMovieList = async () => {
-      const movieList = await fetchMovieList();
-      setMovieList(movieList);
-    };
+    // useEffect を用いて keyword によって fetchMovieList が更新されたら再レンダリングする.
+    useEffect(() => {
+        const loadMovieList = async () => {
+            const movieList = await fetchMovieList();
+            setMovieList(movieList);
+        };
 
-    loadMovieList();
-  }, [fetchMovieList]);
+        loadMovieList();
+    }, [fetchMovieList]);
 
-  return (
-    <div>
-      <div>{keyword}</div>
-      <input type="text" onChange={(e) => setKeyword(e.target.value)} />
-      {movieList
-        .filter((movie) => movie.original_title.includes(keyword))
-        .map((movie) => (
-          <div key={movie.id}>
-            <h2>{movie.original_title}</h2>
-            <img 
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-              alt={movie.original_title} 
-            />
-            <p>{movie.overview}</p>
-          </div>
-        ))
-      }
-    </div>
-  )
+    return (
+        <div>
+            <div>{keyword}</div>
+            <input type="text" onChange={(e) => setKeyword(e.target.value)} />
+            {movieList
+                .filter((movie) => movie.original_title.includes(keyword))
+                .map((movie) => (
+                    <div key={movie.id}>
+                        <h2>{movie.original_title}</h2>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                            alt={movie.original_title}
+                        />
+                        <p>{movie.overview}</p>
+                    </div>
+                ))}
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MovieDetail from "./MovieDetail.tsx";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    { path: "/", Component: App },
+    { path: "/movies/:movieId", Component: MovieDetail },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
